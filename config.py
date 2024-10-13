329f701a321543a6826d8cd4f7a9920b054b35c6
Alex <88356153+Alexxmfs@users.noreply.github.com>
2024-10-13 02:01:39 -0300

    teste

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
+# host = '0.0.0.0'
+# port = 5000
+# conn_str = 'mysql+mysqlconnector://root:root@localhost/onEntreeDB'
+
+
+import os
+
 host = '0.0.0.0'
 port = 5000
-conn_str = 'mysql+mysqlconnector://root:root@localhost/onEntreeDB'
+
+# Use a variável de ambiente DATABASE_URL para se conectar ao banco de dados do Render
+conn_str = os.getenv("DATABASE_URL")
